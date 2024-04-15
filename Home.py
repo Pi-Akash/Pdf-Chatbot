559a6b39de4d210ce1c97e248e392c1a61045e13
Pi Akash <pi.akash96@gmail.com>
2024-04-15 15:34:25 -0400

    Updates
    - Added comments to Home.py file
    - Updated Readme markdown file.
    - updated gitignore

--- Home.py
+++ Home.py
@@ -12,24 +12,35 @@ Steps to perform before running this application:
 
 On the above system configuration, the model takes a while to provide responses.
 """
+
+# imports for program
+
+# streamlit imports for UI
 import streamlit as st
 from streamlit_chat import message
 import os
+
+# UnstructuredPDFLoader to read pdf files
 from langchain_community.document_loaders import UnstructuredPDFLoader
+# OllamaEmbeddings to generate local embedding
 from langchain_community.embeddings import OllamaEmbeddings
+# RecursiveCharacterTextSplitter to split and chunk text
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+# Chroma to connect with ChromaDB
 from langchain_community.vectorstores import Chroma
+# ConversationBufferMemory to keep conversations in buffer memory
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
+# ChatOllama to chat with local model
 from langchain_community.chat_models import ChatOllama
-from langchain.schema import SystemMessage, HumanMessage, AIMessage
-import chromadb
+# to keep track of local embeddings
 import sqlite3
 
 def sqlite_connection(db_name):
     """
     The function creates connection to local sqlite3 db and returns the connection
     """
+    # creates connection with sqlite3 db
     connection = sqlite3.connect(db_name)
     return connection
 
@@ -38,6 +49,7 @@ def get_collection_id(collection_name):
     The function returns collection_id from the local sqlite3 database 
     """
     try:
+        # create connection
         conn = sqlite_connection(db_name = "Embeddings/chroma.sqlite3")
     except Exception as e:
         print(e)
@@ -72,8 +84,10 @@ def get_pdf_text(pdf_doc_filename):
     """
     The function takes in the name of the document and returns the text from the document.
     """
+    # initialize empty text variable
     text = ""
     try:
+        # store text from pdf in text
         pdf_loader = UnstructuredPDFLoader(file_path = os.path.join("Documents", pdf_doc_filename))
         text = pdf_loader.load()
     except Exception as e:
@@ -94,6 +108,10 @@ def get_text_chunks(pdf_data):
     return chunks
 
 def get_hash(text):
+    """
+    Function generates a simple hash based of filename to differentiate between embeddings
+    Helps in memoization of embeddings
+    """
     stripped_text = text.strip().replace(" ", "")
     even_characters = stripped_text[::2]
     reverse_characters = stripped_text[::-1]
@@ -104,8 +122,11 @@ def check_collection_exists(pdf_doc):
     """
     The function checks if a collection already exists for a document.
     """
+    # create document hash
     document_hash_name = get_hash(pdf_doc)
+    # generate collection name
     collection_name = "Embedding_{}".format(document_hash_name)
+    # get collection id
     collection_id = get_collection_id(collection_name)
     if collection_id in os.listdir("Embeddings"):
         #print("Embedding Exists")
@@ -119,7 +140,7 @@ def get_vectorstore(text_chunks, pdf_doc):
     The function takes in text chunks and converts them into embeddings using nomic-embed-text model from ollama.
     And, store the embeddings into ChromaDB and returns those embeddings
     """    
-    st.write(check_collection_exists(pdf_doc))
+    #st.write(check_collection_exists(pdf_doc))
     document_hash_name = get_hash(pdf_doc)
     if check_collection_exists(pdf_doc):
         # load collection from disk
@@ -136,15 +157,19 @@ def get_vectorstore(text_chunks, pdf_doc):
             collection_name = "Embedding_{}".format(document_hash_name),
             persist_directory = "./Embeddings"
         )
+    st.caption("Embeddings Loaded in Memory")
     return vector_db
 
 def get_conversation_chain(vectorstore):
     """
     The function takes in the embeddings to retrieve answers and store the conversation in memory.
     """
+    # load local model
     local_model = "gemma:2b"
     llm = ChatOllama(model = local_model)
+    # create buffer memory object
     conv_memory = ConversationBufferMemory(memory_key = "chat_history", return_messages = True)
+    # create conversation chain using local llm and embeddings in Chroma DB as retriever, store them in memory
     conversation_chain = ConversationalRetrievalChain.from_llm(
         llm = llm,
         retriever = vectorstore.as_retriever(),
@@ -153,11 +178,14 @@ def get_conversation_chain(vectorstore):
     return conversation_chain
 
 def handle_user_input(user_input):
-     response = st.session_state.conversation({"question" : user_input})
-     #st.write(response)
-     st.session_state.chat_history = response["chat_history"]
+    """
+    Function handles user input, generates response and displays chat history with document.
+    """
+    response = st.session_state.conversation({"question" : user_input})
+    #st.write(response)
+    st.session_state.chat_history = response["chat_history"]
      
-     for i, msg in enumerate(st.session_state.chat_history):
+    for i, msg in enumerate(st.session_state.chat_history):
         if i % 2 == 0:
             message(msg.content, is_user = True)
         else:
@@ -168,8 +196,8 @@ def main():
     
     # set page configuration
     st.set_page_config(
-        page_title = "Pdfbot",
-        page_icon = ":cherry_blossom:"
+        page_title = "PDF-Bot",
+        page_icon = ":robot_face:"
     )
 
     if "conversation" not in st.session_state:
@@ -210,7 +238,5 @@ def main():
                 # conversation chain
                 st.session_state.conversation = get_conversation_chain(vectorstore)
           
-    
-                
 if __name__ == "__main__":
     main()
--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # Pdf-Chatbot
- A complete Local RAG app which helps you converse with pdf files.
+A complete Local RAG app which helps you converse with pdf files.
+
+## Introduction
+This is a local RAG application which helps you converse with a pdf file. Idea is to have a bot which can answer questions using information from pdf file as context and provide responses to user in english language. 
+
+One of the use case could be work with financial, medical or confidential reports which are not supposed to be uploaded anywhere.
+
+## Installation
+Since the application runs in local, It expects these dependencies installed prior:
+
+- Download and install Ollama from https://ollama.com/
+- 4. We are using Nomic-Embed-text model from Ollama, follow the below instruction to download model into local
+    ```    
+    ollama pull nomic-embed-text
+    ```
+    5. Similarly, Download the Gemma 2b model from Ollama.
+    ```
+    ollama pull gemma:2b
+    ```
+
+
+## How it works
+1. Clone the repository.
+2. Copy and paste all the documents that you want to work with in the Documents folder, the app will only work with documents which are present in the Documents folder.
+3. Install all the dependencies from 'requirements.txt' file by running the following command:
+```
+pip install -r requirements.txt
+```
+4. Run the below command from the same directory where the Home.py file is located:
+```
+streamlit run /Home.py
+```
+5. The application will launch in your default web browser.
+6. Select the document which you want to converse with and click on the process button.
+7. The app will check if the embeddings for the file exists or not, if not it will create one for the document for later use.
+8. The app will inform you once the embeddings are ready to use.
+9. Now, you can converse with the document using natural language by asking questions.
+
+## Credits
+The initial codebase is inspired from the following tutorials, I highly recommend you to check out awesome developers behind these tutorials and resources:
+
+1. <a href = "https://www.youtube.com/@alejandro_ao">Alejandro AO - Software & Ai</a> : <a href = "https://youtu.be/dXxQ0LR-3Hg">Tutorial Link</a>
+2. <a href = "https://www.youtube.com/@tonykipkemboi">The How-To Guy</a> : <a href = "https://youtu.be/ztBJqzBU5kc">Tutorial Link</a>
+3. <a href = "https://ollama.com/">Ollama</a> : For good repo of models which can be downloaded locally.
+4. <a href = "https://www.langchain.com/">Langchain</a>: For their AI framework for RAG based applications.
+
+## Contribution
+The codebase is free for use and play around with and open to any contribution.
